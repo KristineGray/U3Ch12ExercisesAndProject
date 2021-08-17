a6ef79c6fb4eb117ee8bdc66616fd2edfc144737
KristineGray <krisgraya@gmail.com>
2021-08-17 10:16:25 -0400

    added starter code for exercises & project

---
+++ 12ExerciseBC.py
@@ -0,0 +1,17 @@
+import random
+
+# 12.8.2. Part B: Keys from a Collection
+# 12.8.3. Part C: Modify Values
+
+# Code your assign_tickets function here:
+
+
+# Code the Part C function here:
+
+
+def main():
+  names = ['Caleb', 'Naomi', 'Owen', 'Ava', 'Aaron', 'Lydia']
+
+  
+if __name__ == '__main__':
+  main()
---
+++ 12ExerciseDE.py
@@ -0,0 +1,14 @@
+import string
+
+# 12.8.4. Part D: Counting Characters
+# 12.8.5. Part E: Use a List to Sort Key/Value Output
+
+# Code your character_count function here:
+
+
+def main():
+  text = "Python ROCKS!"
+
+  
+if __name__ == '__main__':
+  main()
