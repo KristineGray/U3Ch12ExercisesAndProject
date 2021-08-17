44ce888c7c50108afb4daac068808e9d118df08f
KristineGray <krisgraya@gmail.com>
2021-08-17 12:00:50 -0400

    completed Part C of 12ExerciseBC

--- 12ExerciseBC.py
+++ 12ExerciseBC.py
@@ -11,13 +11,17 @@ def assign_tickets(name_list):
     return name_dict
 
 # Code the Part C function here:
-
+def fix_tickets(tickets_dict):
+    for (key, value) in tickets_dict.items():
+        if 99 < value < 200:
+            tickets_dict[key] = value + 500
 
 def main():
-  names = ['Caleb', 'Naomi', 'Owen', 'Ava', 'Aaron', 'Lydia']
-  ticket_holders = assign_tickets(names)
-  print(ticket_holders)
-
+    names = ['Caleb', 'Naomi', 'Owen', 'Ava', 'Aaron', 'Lydia']
+    ticket_holders = assign_tickets(names)
+    print(ticket_holders)
+    fix_tickets(ticket_holders)
+    print(ticket_holders)
   
 if __name__ == '__main__':
   main()
